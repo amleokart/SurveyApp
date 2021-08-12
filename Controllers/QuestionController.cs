841e472fb824ccda01b4733137886a6facb3f65c
Your Name <you@example.com>
2021-08-12 18:34:47 +0200

    CRUD operations added for questions and options

--- Controllers/QuestionController.cs
+++ Controllers/QuestionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SurveyApp.Database.DataAccess;
 using SurveyApp.Database.Models;
 
@@ -59,6 +60,38 @@ namespace SurveyApp.Controllers
             _db.Questions.Add(question);
             _db.SaveChanges();
             return RedirectToAction("details", "survey", new { Id = question.SurveyId });
+        }
+
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null || Id == 0) return NotFound("Not found");
+            var question = _db.Questions.Include(q => q.Options).FirstOrDefault(s => s.Id == Id); ;
+            if (question == null) return NotFound("Not found");
+            return View(question);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Question q)
+        {
+            if (ModelState.IsValid)
+            {
+                Question question = _db.Questions.FirstOrDefault(s => s.Id == q.Id); ;
+                question.Content = q.Content;
+                question.Type = q.Type;
+                question.AllowOther = q.AllowOther;
+                question.LastModified = DateTimeOffset.Now;
+                if (((int)q.Type) == 2 || ((int)q.Type) == 3)
+                {
+                    var options = _db.Options.Where(o => o.QuestionId == q.Id);
+                    foreach (var op in options)
+                        _db.Options.Remove(op);
+                    _db.SaveChanges();
+                }
+                _db.SaveChanges();
+                return RedirectToAction("details", "survey", new { Id = q.SurveyId });
+            }
+            return View(q);
         }
     }
 }
